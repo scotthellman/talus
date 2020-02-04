495fcab852b6684b519f09d91c7614c529338d99
Scott Hellman <scott@scutellaris.com>
2020-02-03 20:50:43 -0600

    it's ugly but now surfacing all filtration info

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import talus.morse as morse
 
-node_vals = [(3, 1.5), (-1, 0), (2, 1.1), (8, 3)]
+node_vals = [(0, 3), (1, -1), (2, 10), (3, 2), (4, 7)]
 nodes = [morse.MorseNode(identifier=i, value=v) for i, v in node_vals]
 
 G = nx.Graph()
@@ -9,16 +9,32 @@ for node in nodes:
     G.add_node(node)
 
 G.add_edge(nodes[0], nodes[1])
-G.add_edge(nodes[1], nodes[3])
-G.add_edge(nodes[0], nodes[2])
-G.add_edge(nodes[2], nodes[3])
+G.add_edge(nodes[0], nodes[3])
+G.add_edge(nodes[1], nodes[2])
+G.add_edge(nodes[1], nodes[4])
+G.add_edge(nodes[3], nodes[4])
 
 print(G.nodes)
 print(G.edges)
 
-result = morse.persistence(G)
-
-assert(result[-1] == 0)
-assert(abs(result[3] - 0.4) < 0.00001)
+(result, filtration, morse) = morse.persistence(G)
 
 print(result)
+print(filtration)
+print(morse)
+
+crystals = {k: [] for k, v in result.items() if v > 0}
+
+for node in morse:
+    crystals[node[1]].append(node[0])
+
+print("Full morse complex:")
+print(crystals)
+print("-" * 10)
+
+for time, destroyed, parent in filtration:
+    crystals[parent].extend(crystals[destroyed])
+    del crystals[destroyed]
+    print("At lifetime", time)
+    print(crystals)
+    print("-" * 10)
