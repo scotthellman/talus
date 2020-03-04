fd4f5e1e90479121829bb80d7e8917f5c48dc8e7
Scott Hellman <scott@scutellaris.com>
2020-03-04 17:31:27 -0600

    roll all functionality into the python class

--- talus/morse.py
+++ talus/morse.py
@@ -32,6 +32,14 @@ class MorseSmaleData:
 
 
 class MorseData:
+    """ Stores the results of the Morse complex computation.
+
+    Surfaces three attributes:
+    `lifetimes`: A map of each variable ID to its persistence value.
+    `filtration`: A list containing the times at which each partition is merged
+        into its parent, alongside the ID of that parent.
+    `assignments`: A map of each variable ID to its partition.
+    """
 
     def __init__(self, rust_output):
         self.lifetimes = rust_output[0]
@@ -39,49 +47,80 @@ class MorseData:
         self.assignments = {a[0]: a[1] for a in rust_output[2]}
 
     def compute_cells_at_lifetime(self, lifetime: float):
-        crystals = {k: [] for k, v in self.lifetimes.items() if v > 0}
+        # FIXME: This isn't really correct. It's partitions that survived for at least `lifetime` length
+        # The docstring kind of implies that they shows up at lifetime, which is a separate concept
+        """ Returns the partitions that survive for at least `lifetime`.
+
+        Any partitions that survive for shorter than `lifetime` will be merged into partitions
+        that do survive, according to the Morse filtration
+        """
+        cells = {k: [] for k, v in self.lifetimes.items() if v > 0}
         for node, parent in self.assignments.items():
-            crystals[parent].append(node)
+            cells[parent].append(node)
 
         # NOTE: filtration is in sorted order
         for step in self.filtration:
             if lifetime is not None and step.lifetime > lifetime:
                 break
-            crystals[step.owning_id].extend(crystals[step.destroyed_id])
-            del crystals[step.destroyed_id]
-        return crystals
+            cells[step.owning_id].extend(cells[step.destroyed_id])
+            del cells[step.destroyed_id]
+        return cells
 
 
 class MorseSmaleComplex:
+    """ Computes the Morse complex given a set of points and their values.
 
-    def __init__(self, k=8, approximate=False):
+    This is intended to be a sklearn-like API into the Morse code.
+
+    `k`: The number of neighbors used when constructing the underlying kNN graph
+    `approximate`: If true, computes the approximate kNN graph. Otherwise, computes
+        the exact kNN graph.
+    `sample_rate`: Controls how many new edge are considered at each step in the
+        approximate graph construction. Has no effect in `approximate` is `False`.
+    `precision`: The lower this value, the stricter the convergence criterion for
+        approximate graph construction. Has no effect in `approximate` is `False`.
+
+    If you already have a graph, a MorseSmaleComplex can be constructed using `from_graph`.
+    """
+
+    def __init__(self, k=8, approximate=False, sample_rate=0.5, precision=0.001):
         self.k = k
         self.approximate = approximate
+        self.sample_rate = sample_rate
+        self.precision = precision
+
+    @staticmethod
+    def from_graph(graph):
+        ms_complex = MorseSmaleComplex(None, None)
+        ms_complex.morse_data = MorseSmaleComplex._persistence(graph)
+        return ms_complex
+
+    @staticmethod
+    def _persistence_by_knn(points, k: int):
+        data = talus._persistence_by_knn(points, k)
+        return MorseSmaleData(data)
+
+    @staticmethod
+    def _persistence_by_approximate_knn(points, k, sample_rate, precision):
+        data = talus._persistence_by_approximate_knn(points, k, sample_rate, precision)
+        return MorseSmaleData(data)
+
+    @staticmethod
+    def _persistence(graph):
+        nodes = [n for n in graph]
+        edges = [(e[0].identifier, e[1].identifier) for e in graph.edges]
+        data = talus._persistence(nodes, edges)
+        return MorseSmaleData(data)
 
     def fit_transform(self, X, labels):
         morse_points = []
         for i, (point, label) in enumerate(zip(X, labels)):
             morse_points.append(MorseNode(identifier=i, value=label, vector=list(point)))
         if self.approximate:
-            self.morse_data = persistence_by_approximate_knn(morse_points, k=self.k)
+            self.morse_data = self._persistence_by_approximate_knn(morse_points, k=self.k,
+                                                                   sample_rate=self.sample_rate,
+                                                                   precision=self.precision)
         else:
-            self.morse_data = persistence_by_knn(morse_points, k=self.k)
+            self.morse_data = self._persistence_by_knn(morse_points, k=self.k)
         return self.morse_data
 
-
-
-def persistence(graph):
-    nodes = [n for n in graph]
-    edges = [(e[0].identifier, e[1].identifier) for e in graph.edges]
-    data = talus._persistence(nodes, edges)
-    return MorseSmaleData(data)
-
-
-def persistence_by_knn(points, k: int):
-    data = talus._persistence_by_knn(points, k)
-    return MorseSmaleData(data)
-
-
-def persistence_by_approximate_knn(points, k, sample_rate, precision):
-    data = talus._persistence_by_approximate_knn(points, k, sample_rate, precision)
-    return MorseSmaleData(data)
--- test.py
+++ test.py
@@ -1,6 +1,27 @@
 import networkx as nx
 import talus.morse as morse
 
+# TODO: Make these be proper tests instead of a thrown together script
+
+
+def evaluate_graph(G):
+    ms_complex = morse.MorseSmaleComplex.from_graph(G)
+    result = ms_complex.morse_data
+
+    print(result.ascending_complex.assignments)
+    print(result.ascending_complex.lifetimes)
+
+    #print(result.descending_complex.compute_cells_at_lifetime(0))
+    #for f in result.descending_complex.filtration:
+    #    print(result.descending_complex.compute_cells_at_lifetime(f.lifetime))
+
+    print(result.ascending_complex.compute_cells_at_lifetime(0))
+    for f in result.ascending_complex.filtration:
+        print(result.ascending_complex.compute_cells_at_lifetime(f.lifetime))
+    print("-"*10)
+
+
+# This one is just a real basic sort of graph
 node_vals = [(0, 3), (1, -1), (2, 10), (3, 2), (4, 7)]
 nodes = [morse.MorseNode(identifier=i, value=v) for i, v in node_vals]
 
@@ -14,17 +35,20 @@ G.add_edge(nodes[1], nodes[2])
 G.add_edge(nodes[1], nodes[4])
 G.add_edge(nodes[3], nodes[4])
 
-print(G.nodes)
-print(G.edges)
+evaluate_graph(G)
 
-result = morse.persistence(G)
+# Identical values in same morse cell
+node_vals = [(0, 1), (1, 1), (2, 1), (3, -1), (4, 10)]
+nodes = [morse.MorseNode(identifier=i, value=v) for i, v in node_vals]
 
-print(result)
+G = nx.Graph()
+for node in nodes:
+    G.add_node(node)
 
-print(result.descending_complex.compute_cells_at_lifetime(0))
-for f in result.descending_complex.filtration:
-    print(result.descending_complex.compute_cells_at_lifetime(f.lifetime))
+G.add_edge(nodes[0], nodes[1])
+G.add_edge(nodes[0], nodes[2])
+G.add_edge(nodes[1], nodes[3])
+G.add_edge(nodes[2], nodes[3])
+G.add_edge(nodes[3], nodes[4])
 
-print(result.ascending_complex.compute_cells_at_lifetime(0))
-for f in result.ascending_complex.filtration:
-    print(result.ascending_complex.compute_cells_at_lifetime(f.lifetime))
+evaluate_graph(G)
