cdf5c6918466e404eda6864f83c90749dd178c66
Scott Hellman <scott@scutellaris.com>
2020-03-04 17:31:27 -0600

    roll all functionality into the python class

--- bench.py
+++ bench.py
@@ -4,18 +4,18 @@ import talus.morse as morse
 import numpy as np
 
 
-def build_nodes(n):
+def build_data(n):
     sampled = np.random.random((n, 3))
-    points = []
-    for i, row in enumerate(sampled):
-        points.append(morse.MorseNode(identifier=i, value=row[0], vector=list(row[1:])))
-    return points
+    return sampled[:, 1:], sampled[:, 0]
 
 
 for size in [500000]:
     print(size)
-    points = build_nodes(size)
-    result = morse.persistence_by_approximate_knn(points, 8, 0.8, 0.001)
+    X, y = build_data(size)
+    print(X.shape)
+    print(y.shape)
+    ms_complex = morse.MorseSmaleComplex(k=8, approximate=True)
+    ms_complex.fit_transform(X, y)
     1/0
     start = time.time()
     result = morse.persistence_by_knn(points, 8)
